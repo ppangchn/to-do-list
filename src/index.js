985571e513dcca2359c11d91821398cf2ad45103
Chanissa Trithipkaiwanpon <pang.chanissa@gmail.com>
2019-12-06 00:50:50 +0700

    add apollo-cache dependency

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 import './index.css';
@@ -9,6 +10,7 @@ import 'antd/dist/antd.css';
 
 const client = new ApolloClient({
 	uri: 'http://localhost:4000',
+	cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
